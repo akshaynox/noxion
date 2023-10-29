054f15ae1c2958c76c626d36a21a44658c150350
Akshay <aksingh240@gmail.com>
2023-10-29 20:48:49 +0530

    added marketing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,7 @@
+import { Navbar } from "./_components/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full">{/* <Navbar /> */}</div>;
+};
+
+export default MarketingLayout;
