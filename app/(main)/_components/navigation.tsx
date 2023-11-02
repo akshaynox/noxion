8f80fbe913ab5b3b87fa63f3fef934be655bbf51
Akshay <aksingh240@gmail.com>
2023-11-02 22:34:54 +0530

    begin sidenav

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,3 +1,31 @@
+import { ChevronsLeft } from "lucide-react";
+
 export const Navigation = () => {
-  return <div>navigation</div>;
+  return (
+    <>
+      <aside
+        className="group/sidebar h-full bg-secondary overflow-y-auto
+        relative flex w-60 flex-col z-[99999]"
+      >
+        <div
+          role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300
+           dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 
+           group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div
+          className="opacity-0 group-hover/sidebar:opacity-100 transition
+          cursor-ew-resize absolute h-full right-0 top-0 w-1 bg-primary/10"
+        />
+      </aside>
+    </>
+  );
 };
